a51f7281d2ff18e8e9b55f25334245efb92bc108
magnusstampe <m_s_nielsen288@hotmail.com>
2018-06-03 21:42:00 +0200

    fixes

--- includes.js
+++ includes.js
@@ -15,7 +15,6 @@ async function hentHeader() {
     let headerData = await fetch("header.html");
     let header = await headerData.text();
     document.querySelector("header.include").innerHTML = header;
-    document.querySelector("header.include").parentNode.appendChild(document.querySelector("header.include"));
 };
 
 //Nav
@@ -23,7 +22,6 @@ async function hentNav() {
     let navData = await fetch("nav.html");
     let nav = await navData.text();
     document.querySelector("nav.include").innerHTML = nav;
-    document.querySelector("nav.include").parentNode.appendChild(document.querySelector("nav.include"));
 
     function toggleMenu() {
         document.querySelector("nav.include").classList.toggle("open");
@@ -41,7 +39,4 @@ async function hentFooter() {
     let footerData = await fetch("footer.html");
     let footer = await footerData.text();
     document.querySelector("footer").innerHTML = footer;
-    document.querySelector("footer").parentNode.appendChild(document.querySelector("footer"));
-
-
 };
